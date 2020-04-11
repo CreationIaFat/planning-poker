556a0aeed6489aae3163139d06dd45264704ceae
Creation Is Fat <freestyle.baller.sk8er@gmail.com>
2020-04-11 17:27:57 +0900

    add コンソールログの追加及びidの活用

---
+++ js/letTest.js
@@ -0,0 +1,4 @@
+let myFavorite1 = "たこ焼き"; //constは一か所しか使わない
+myFavoriteComent = "大好き"// let は再代入可能
+myFavoriteComent += "塩マヨネーズ"
+console.log(myFavorite1 + "太たこ" + myFavoriteComent);
---
+++ js/whatIsGood.js
@@ -0,0 +1,2 @@
+const whatIsGood = "What is good ?"
+document.querySelector("#whatIsGood1").textContent = whatIsGood;
