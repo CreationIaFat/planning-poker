737cc96921bd5ed7ce15e4321c4471ec3b6a8a73
sotahMac <freestyle.baller.sk8er@gmail.com>
2020-04-12 10:54:07 +0900

    add 配列で表示する方法のパターンを追加(findと=>とfunction)

--- js/tablePlannningPoker.js
+++ js/tablePlannningPoker.js
@@ -4,11 +4,36 @@ let amountOfArray = 6; // フィボナッチで見積もりたい選択肢の数
 let planningPoker = [0,1,2];
 console.log("初期の配列の値は[" + planningPoker + "]です。ここから");
 console.log( "配列の中身が" + amountOfArray + "個になるまで足します。")
+
+
+// // パターン1：シンプルにで配列を表示する方法
+// for( let i = 4; i <= amountOfArray; i++){
+//     let cnt = planningPoker[planningPoker.length -1]
+//     let precnt = planningPoker[planningPoker.length -2];
+//     let fibonatch = cnt + precnt;
+//     planningPoker.push(fibonatch);
+//     console.log(i + "番の配列の値は" + planningPoker[planningPoker.length - 1]); //ここの違い
+// }
+// console.log("よって最終的な配列[" + planningPoker + "]")
+
+
+// パターン2：　findとallow関数を用いて配列を表示する方法
 for( let i = 4; i <= amountOfArray; i++){
     let cnt = planningPoker[planningPoker.length -1]
     let precnt = planningPoker[planningPoker.length -2];
     let fibonatch = cnt + precnt;
     planningPoker.push(fibonatch);
-    console.log(i + "番の配列の値は" + planningPoker[planningPoker.length - 1]);
+    console.log(i + "番の配列の値は" + planningPoker.find(a => a[a.length - 1])); //ここの違い
 }
-console.log("よって最終的な配列[" + planningPoker + "]")
+console.log("よって最終的な配列[" + planningPoker + "]");
+
+
+// // パターン3: findとfunctionを用いて配列を表示する方法
+// for( let i = 4; i <= amountOfArray; i++){
+//     let cnt = planningPoker[planningPoker.length -1]
+//     let precnt = planningPoker[planningPoker.length -2];
+//     let fibonatch = cnt + precnt;
+//     planningPoker.push(fibonatch);
+//     console.log(i + "番の配列の値は" + planningPoker.find(function(a){ return a[a.length - 1]})); //ここの違い
+// }
+// console.log("よって最終的な配列[" + planningPoker + "]");
