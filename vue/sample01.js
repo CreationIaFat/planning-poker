8421b68c240aa44d9dab25a50844c9a5edc554f7
sotahMac <freestyle.baller.sk8er@gmail.com>
2020-05-11 00:21:27 +0900

    add Mustache構文,v-bind, v-for

--- vue/sample01.js
+++ vue/sample01.js
@@ -1,7 +1,22 @@
 /* eslint-disable */
 new Vue({
+    // Vue.jsの本体を読み込むとVue関数を使用できるようになる
+    // Vueアプリを作成し、起動するにはVue関数を用いてVueインスタンスを作成する
+    // Vue関数は引数としてオプションオブジェクトを受け取り、そのオプションオブジェクトの内容によりVueアプリの挙動が変わる
+    //
+    //　オプションオブジェクト詳細
+    // https://vuejs.org/v2/api/index.html#props
+    //
+    // ここにvue.jsの設定を行うためのオブジェクトをかく
     el: "#app",
     data: {
-        message: "Hello, Vue.js"
+        message: "Hello, Vue.js",
+        AAA: "https://vuejs.org/v2/api/index.html#props",
+        isFoo: true,
+        isBar: false,
+        bazClass: "baz",
+        inStock: 0,
+        // 配列の表示はダブルコーテーションで囲まないと表示されない↓
+        items: ["ねこ", "犬なのに", "名前は", "ねこw", "ふふふw"]
     }
 })
