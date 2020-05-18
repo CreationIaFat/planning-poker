2c33eff1511aa2ca3925ab55b6690f41a4f03c16
sotahMac <freestyle.baller.sk8er@gmail.com>
2020-05-18 23:38:05 +0900

    fix CRUDのReadにコメントを追加

--- js/script.js
+++ js/script.js
@@ -7,13 +7,24 @@ let inputForm, todoMain, tabButton, sortMenu
 
 /** Todo一個単位のHTML文字列を生成する */
 function createTodoHtmlString(todo) {
+  // HTML文字列をプールする関数
   let htmlString = "";
+  // HTMLのdata属性に設定する編集中を判定する内容
   const editType = todo.isEdit ? "editFixed" : "edit";
+  // ボタンラベルを編集中かどうかで分岐する
   const editButtonLabel = todo.isEdit ? "編集完了" : "編集";
+  // HTMLのdata属性に設定する完了したかどうかを判定する内容
   const doneType = todo.isDone ? "inbox" : "done";
+  // ボタンのラベルを未完了か完了で分岐する
   const doneButtonLabel = todo.isDone ? "未完了" : "完了";
-  let todoTextCell, priorityCell;
+  // todoテキストが入るテーブルセルHTML文字列をプールする変数
+  let todoTextCell = "";
+  // 優先度テキストが入るテーブルセルHTML文字列をプールする変数
+  let priorityCell = "";
+  // 編集中かそうでないかで描画するHTMLを分岐する
   if (todo.isEdit) {
+    // 該当のtodoオブジェクトが編集中の場合はテキストフィールドを描画する
+    // テキストフィールドなのでユーザは文字や数値を変更できるようにする
     todoTextCell =
       '<td class="cell-text"><input class="input-edit" type="text" value=' +
       todo.text +
@@ -23,29 +34,42 @@ function createTodoHtmlString(todo) {
       todo.priority +
       " /></td>";
   } else {
+    // 通常時の状態
+    // ユーザは情報を見るだけなので普通のテキストとして表示すればOK
     todoTextCell = '<td class="cell-text">' + todo.text + "</td>";
     priorityCell = '<td class="cell-priority">' + todo.priority + "</td>";
   }
+  // Todoオブジェクト1つにつき1行なので、行を生成するtrタグをまず作る
   htmlString += '<tr id="' + todo.id + '">';
+  // 編集中を判定するための文字列をdata属性に埋め込んでボタンを作る
+  // 非編集時は編集ボタンを、編集中は編集完了ボタンとなる
   htmlString +=
     '<td class="cell-edit-button"><button data-type="' +
     editType +
     '">' +
     editButtonLabel +
     "</button></td>";
+    // 先に生成したTodoの文字列情報
   htmlString += todoTextCell;
+  // Todoオブジェクトの作成日
   htmlString += '<td class="cell-created-at">' + todo.createdAt + "</td>";
+  // 優先度
   htmlString += priorityCell;
+  // 完了ボタンのセルを作る
   htmlString += '<td class="cell-done">';
+  // Todoオブジェクトの完了状態を文字列としてdata属性に埋め込む
   htmlString += '<button data-type="' + doneType + '">';
+  // 完了かそうでないかをボタンのラベルに表示する
   htmlString += doneButtonLabel;
   htmlString += "</button></td>";
   htmlString += "</tr>";
+  // 作ったHTMLを返す
   return htmlString;
 }
 
 /** TodoListの描画を更新する */
 function updateTodoList() {
+  // HTML文字列をプールする変数
   let htmlStrings = "";
   // HTMLを書き換える
   todoList.forEach(todo => {
@@ -115,6 +139,7 @@ function bindEvents() {
 function initialize() {
   registerDOM();
   bindEvents();
+  updateTodoList();
 }
 
 // DOMContentLoaded イベントは、ブラウザがページの解析を完了した段階で <<<一度だけ>>> 発火するイベント
